16ce5911d0e82d284110f5b80c57452ee443463a
JoseOtavioSantin <57141195+JoseOtavioSantin@users.noreply.github.com>
2025-07-17 09:42:56 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,6 +1,6 @@
-// js/auth.js
-import { auth } from "./firebase.js";
+import { auth, db } from "./firebase.js";
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
 window.login = async function () {
   const email = document.getElementById("email").value;
@@ -8,11 +8,24 @@ window.login = async function () {
 
   try {
     await signInWithEmailAndPassword(auth, email, senha);
+    
+    // Buscar filial do usuário no Firestore
+    const docRef = doc(db, "usuarios", email);
+    const docSnap = await getDoc(docRef);
+
+    if (!docSnap.exists()) {
+      alert("Erro: usuário sem filial definida.");
+      return;
+    }
+
+    const filial = docSnap.data().filial;
+
+    // Salva o e-mail e a filial no localStorage
     localStorage.setItem("usuarioLogado", email);
+    localStorage.setItem("filialLogada", filial);
+
     window.location.href = "dashboard.html";
   } catch (error) {
     alert("Erro ao fazer login: " + error.message);
   }
 };
-
-
